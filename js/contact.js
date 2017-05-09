ca61295b7de1af522a7c182f55a10c1d43da70e8
Peter Tascio <peter.tascio@gmail.com>
2017-05-09 14:16:50 -0400

    Form validations

--- js/contact.js
+++ js/contact.js
@@ -7,19 +7,61 @@ var submitButton;
 var filledName = "";
 var filledEmail = "";
 var filledMessage = "";
+
 function handleInput(e){
   var elid = e.target.id;
+  var elval = e.target.value;
   if (elid === "contact-name"){
-    filledName = e.currentTarget.value;
+    filledName = elval;
   }else if (elid === "contact-email"){
-    filledEmail = e.currentTarget.value;
+    filledEmail = elval;
   }else if (elid === "contact-message"){
-    filledMessage = e.currentTarget.value;
+    filledMessage = elval;
   }
 }
 
+function displayErrors(elid){
+  var el = document.getElementById(elid);
+  el.innerText = "*Entry Cannot Be Blank";
+}
+
+function displayEmailErrors(elid){
+  var el = document.getElementById("email-errors");
+  el.innerText = "*Not a valid email";
+}
+
+function removeErrors(elid){
+  var el = document.getElementById(elid);
+  el.innerText = "";
+}
+
 function checkValues(){
-  debugger
+  if (filledName.length < 1){
+    displayErrors("name-errors");
+  }else {
+    removeErrors("name-errors");
+  }
+  if (filledEmail.length < 1){
+    displayErrors("email-errors");
+  }else if (filledEmail.length > 0){
+    checkEmail(filledEmail);
+  }
+  if (filledMessage.length < 1){
+    displayErrors("message-errors");
+  }else {
+    removeErrors("message-errors");
+  }
+}
+
+function checkEmail(email){
+  var uppercasePassword = email.toUpperCase();
+  var re = new RegExp("^([A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,})$");
+  if (!re.test(uppercasePassword)){
+    displayEmailErrors("email-errors");
+  }else {
+    removeErrors("email-errors");
+  }
+  return re.test(uppercasePassword);
 }
 
 
